bca4127d294730950b6808a15e0abffd8f2df1e3
Saravanan <saran96008871@gmail.com>
2024-02-20 14:51:55 +0530

    Commit No 2

--- src/index.js
+++ src/index.js
@@ -3,18 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom';
-import { Provider } from 'react-redux';
-import store from './components/Redux/store/store';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
+    
     <BrowserRouter>
       <App />
     </BrowserRouter>
-    </Provider>
+ 
   </React.StrictMode>
 );
 
